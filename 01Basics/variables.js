10d2091136eca24bc8afb56a3397f0798cfa6e1f
Georgeo <georgeosabu@gmail.com>
2019-11-10 13:08:22 +0200

    variables and constants

---
+++ 01Basics/variables.js
@@ -0,0 +1,11 @@
+let name ='john'
+console.log(name);
+let score=100
+console.log(score)
+const bonus=20
+let totalScore=score+bonus
+console.log(totalScore)
+let firstName='john'
+firstName='jane'
+let sName='doe'
+console.log(firstName+' '+sName)
