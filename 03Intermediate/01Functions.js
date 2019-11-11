936a483548a14490d12b27030470848c7108ee9d
Georgeo <georgeosabu@gmail.com>
2019-11-11 13:15:12 +0200

    default parameters in functions

--- 03Intermediate/01Functions.js
+++ 03Intermediate/01Functions.js
@@ -13,7 +13,19 @@ let myAdder=function(numOne,numeTwo){
     return added
 }
 
+let myMulti=function(numOne,numeTwo){
+    return numOne*numeTwo
+    
+}
+
+let guestUser=function(name='unName',courseCount=0){
+    return 'Hello '+name+' your course count is '+courseCount
+}
+
 sayHello('John')
 fullNameMaker('John','Doe')
 let answer=myAdder(5,5)
 console.log(answer)
+console.log(myMulti(5,5))
+
+console.log(guestUser('John',6))
