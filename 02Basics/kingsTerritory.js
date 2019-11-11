e3850bed3065f64e8754a61125c0f85645f36601
Georgeo <georgeosabu@gmail.com>
2019-11-11 11:38:40 +0200

    Dangers of javaScript without declaring a Variable

---
+++ 02Basics/kingsTerritory.js
@@ -0,0 +1,19 @@
+//Kings problem
+
+//let king ='John'
+
+if(true){
+    //let king ='Sam'
+
+    if(true){
+         king ='Richard' //badbabdbad
+        console.log(king)
+    }
+}
+
+if(true){
+    console.log("2"+king)
+}
+
+//Dangers of JavaScript
+//if you havent declared a variable with let or var then you are automatically classifies as a a global variable.
