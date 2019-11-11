67b3f7743558127aca811e4f716c72107d9e0542
Georgeo <georgeosabu@gmail.com>
2019-11-11 11:57:37 +0200

    Arrays and templating strings

---
+++ 02Basics/marvels.js
@@ -0,0 +1,7 @@
+//Data type -- Arrays
+
+const superheros=['IronMan','SpiderMan','CaptainAmerica']
+console.log(superheros)
+console.log(superheros[0])
+console.log(superheros[superheros.length-1])
+console.log(`We have  ${superheros.length} superheros`)
