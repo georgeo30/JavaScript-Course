6360e41292f48994211277acdfbbc0fa88deb130
Georgeo <georgeosabu@gmail.com>
2019-11-12 09:39:02 +0200

    for loop

--- 03Intermediate/trello.js
+++ 03Intermediate/trello.js
@@ -1,22 +1,25 @@
 const days=['monday','tuesday','wednesday','thursday','friday','saturday']
 
 
-console.log(days[0])
+// console.log(days[0])
 
 
-days.forEach(function(day,index){
-    console.log(index)
-    if(day==='monday'||day==='wednesday'){
-        console.log('You have to attend a meeting at 9 am')
-        console.log(days.shift())
+// days.forEach(function(day,index){
+//     console.log(index)
+//     if(day==='monday'||day==='wednesday'){
+//         console.log('You have to attend a meeting at 9 am')
+//         console.log(days.shift())
         
-    }
-    else if(day==='tuesday'){
-        console.log('dont go to work today')
-    }
-    else{
-        console.log('Work at 8 am')
-    }
-})
+//     }
+//     else if(day==='tuesday'){
+//         console.log('dont go to work today')
+//     }
+//     else{
+//         console.log('Work at 8 am')
+//     }
+// })
 
-console.log(days)
+// console.log(days)
+for(let i=days.length-1;i>=0;i--){
+    console.log(days[i])
+}
