abc11b65ac1c74b62f97442b38466f18b804762e
Georgeo <georgeosabu@gmail.com>
2019-11-12 11:14:17 +0200

    basics objects

---
+++ 04Intermediate/myObjectss.js
@@ -0,0 +1,21 @@
+let myYoutubeVideo={
+    title: 'Loops in javaScript',
+    videoLength: '15',
+    videoCreator: 'Georgeo',
+    videoDescription: 'this is a video description and a long one'
+
+
+}
+
+console.log(`hey new vide on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`)
+
+
+let courseDescrp={
+    courseN:    'JavaScript',
+    courseP:    'R1233',
+    courseD:    'JavaScript Fundementals for beginners'
+    
+    
+}
+
+console.log(`Would you like to purchase the ${courseDescrp.courseN} course which is about ${courseDescrp.courseD} for ${courseDescrp.courseP} ?`)
