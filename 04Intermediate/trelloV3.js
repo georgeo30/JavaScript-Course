462d3bbcad4a39385a1d6897e1a4ce45f87c18a2
Georgeo <georgeosabu@gmail.com>
2019-11-12 21:36:41 +0200

    object methods for trello

--- 04Intermediate/trelloV3.js
+++ 04Intermediate/trelloV3.js
@@ -2,20 +2,39 @@ let myTodos={
     day: 'monday',
     meetings: 0,
     meetDone: 0,
+    remainingMeeting: 0,
 
     addMeeting: function(num){
         this.meetings=this.meetings+num
+        this.remainingMeeting=this.remainingMeeting+num;
     },
 
     summary: function(){
-        return `You have ${this.meetings} meetings today`
-    }
+        return {
+            ayyo: `You have ${this.meetings} meetings today`,
+            ayy: `You had ${this.meetDone} meetings today`,
+            ay:     `You have ${this.remainingMeeting} meetings remaining today`,
+        }
+    },
+
+    meetingDone: function(meet){
+        this.meetDone=this.meetDone+meet
+        this.remainingMeeting=this.remainingMeeting-meet
+    },
+    
+    reset: function(){
+        this.meetings=0;
+        this.meetDone=0;
+    },
 }
 
 
 
 myTodos.addMeeting(4)
-console.log(myTodos.summary())
+myTodos.meetingDone(3)
+console.log(myTodos.summary().ayyo)
+console.log(myTodos.summary().ayy)
+console.log(myTodos.summary().ay)
 
 //assigment
 //handle meetings done
