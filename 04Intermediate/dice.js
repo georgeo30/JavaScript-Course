1affc23c4cb841786ad5fda64eff49dff0ae42ab
Georgeo <georgeosabu@gmail.com>
2019-11-13 09:30:10 +0200

    using the math library and random numbers

---
+++ 04Intermediate/dice.js
@@ -0,0 +1,16 @@
+let numOne=22
+let numTwo=7
+
+result =22/7
+// console.log(result.toFixed(3))
+// let floatRes=result.toFixed(3)
+
+
+// console.log(Math.floor(floatRes))
+// console.log(Math.ceil(floatRes))
+
+let lower=1
+let upper=6
+
+let myRand=Math.floor(Math.random()*(upper - lower+1))+lower
+console.log(myRand)
