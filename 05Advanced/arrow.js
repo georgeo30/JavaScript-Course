e2f583e005b679a8c834bc98a1dff4fe8650b566
Georgeo <georgeosabu@gmail.com>
2019-11-13 17:38:40 +0200

    simple arrow function

---
+++ 05Advanced/arrow.js
@@ -0,0 +1,10 @@
+// const sayHello=function(name){
+//     return "Hey there, "+ name + " !"
+// }
+
+// console.log(sayHello('george'))
+
+const sayHello=(name) => `Hey there ${name}! `
+
+
+console.log(sayHello('george'))
