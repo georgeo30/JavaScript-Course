d96a360f3e8b842f84544f2be680fd8d4789ed69
Georgeo <georgeosabu@gmail.com>
2019-11-14 16:06:49 +0200

    maps

---
+++ 05Advanced/mapsAndForOf.js
@@ -0,0 +1,26 @@
+var john={
+    name:   'I am John',
+    age:    24,
+    isActive:   true,
+
+}
+
+var marry={
+    name:   'I am Mary',
+    age:    23,
+    isActive:   true,
+}
+
+var sam={
+    name:   'I am Sam',
+    age:    29,
+    isActive:   false,
+}
+
+let users=new Map()
+
+users.set('john',john)  //'john'(first parameter) is the key, john(second param iss what is beig mapped to the key)
+users.set('marry',marry)
+users.set('sam',sam) 
+
+console.log(users)
